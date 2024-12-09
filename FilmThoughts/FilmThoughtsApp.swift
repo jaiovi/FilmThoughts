a01226b2ed5a93b077cf28a49e47302d7f5b105c
Jesús Sebastián Jaime Oviedo <sebastianjo2011@gmail.com>
2024-12-09 12:24:41 +0100

    Initial Commit

---
+++ FilmThoughts/FilmThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmThoughtsApp.swift
+//  FilmThoughts
+//
+//  Created by Jesus Sebastian Jaime Oviedo on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
